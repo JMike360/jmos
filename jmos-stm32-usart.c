edf96316dd6fa5551e61309e52ee79b6ab7972e1
John Michael Bolinder <reachjohnm@gmail.com>
2023-08-10 20:07:03 -0700

    moving USART functions to a dedicated file, implementing simple echo

---
+++ jmos-stm32-usart.c
@@ -0,0 +1,47 @@
+#include "jmos-stm32-usart.h"
+
+void init_usart(){
+	// need to first make sure it's off
+	RCC->APB2ENR &= ~USART_EN;
+
+	// --- Configure the USART1 GPIO block --- //
+    RCC->AHBENR |= GPIOA_EN;
+    
+    uint32 moder_val = (USART_ALTFN << RX_PIN*2) | (USART_ALTFN << TX_PIN*2);       
+    GPIOA->MODER &= ~moder_val;
+	GPIOA->MODER |= moder_val; // MODER, because alternate gpio fns :)
+                                                                       
+	uint32 tx_afr_offset = (TX_PIN % 8) * AFR_SEL_SIZE;
+	uint32 rx_afr_offset = (RX_PIN % 8) * AFR_SEL_SIZE;
+	
+    uint32 afrh_val = (AFR_USART << tx_afr_offset) | (AFR_USART << rx_afr_offset);
+    GPIOA->AFRH &= ~afrh_val;
+    GPIOA->AFRH |= afrh_val;
+     
+	RCC->APB2ENR |= USART_EN; 
+                                                                                                                                                                                                    
+    // --- Configure Baud to 9600 --- //
+    USART1->BRR = 0x1368; // baud = fclk / BRR ::: 0x1388 * 0d9600 = fclk
+ 
+    // --- Enable basic fns in USART1 register --- //
+    uint32 cr1_val = TX_EN | RX_EN | UE_EN;
+    USART1->CR1 &= ~cr1_val;
+    USART1->CR1 |= cr1_val;
+}
+
+int putchar(unsigned char c){
+    while((USART1->ISR & USART_TXE) != USART_TXE); // wait for TDR to be empty
+    USART1->TDR = (uint32)c;
+    return c;
+}
+
+int finish_tx(){
+   while((USART1->ISR & USART_TC) != USART_TC);
+   return 0;
+}
+
+unsigned char getchar(){
+    while((USART1->ISR & USART_RXNE) != USART_RXNE);
+    uint32 c = USART1->RDR;
+    return (unsigned char)c;
+}
--- main.c
+++ main.c
@@ -1,7 +1,6 @@
 #include "jmos-stm32-init.h"
 #include "jmos-stm32-periph.h"
-#define HIGH 1
-#define LOW 0
+#include "jmos-stm32-usart.h"
 
 void init_led3(){
     // unsigned long int RCC = 0x40021000;
@@ -65,113 +64,6 @@ void set_led3_off(){
     return;
 }
 
-void init_usart(){
-    // need to first make sure it's off
-    uint32 USART_EN = 0x00004000;
-    RCC->APB2ENR &= ~USART_EN;
-
-    // --- Configure the USART1 GPIO block --- //
-    uint32 GPIOA_EN = 0x00020000;
-    RCC->AHBENR |= GPIOA_EN;
-
-    uint32 altfn = 0x02; //? documentation is a bit unclear what the alternate fn value should be, I think this is right tho
-    uint32 rxPin = 10;
-    uint32 txPin = 9;
-    uint32 moder_val = (altfn << rxPin*2) | (altfn << txPin*2);
-
-    GPIOA->MODER &= ~moder_val;
-    GPIOA->MODER |= moder_val; // MODER, because alternate gpio fns :)
-
-    uint32 afr_usart = 0x01;
-    int afr_sel_size = 4;
-    uint32 tx_afr_offset = (txPin % 8) * afr_sel_size;
-    uint32 rx_afr_offset = (rxPin % 8) * afr_sel_size;
-
-    uint32 afrh_val = (afr_usart << tx_afr_offset) | (afr_usart << rx_afr_offset);
-    GPIOA->AFRH &= ~afrh_val;
-    GPIOA->AFRH |= afrh_val;
-
-    RCC->APB2ENR |= USART_EN;
-    
-    // --- Configure Baud to 9600 --- //
-    USART1->BRR = 0x1370; // baud = fclk / BRR ::: 0x1388 * 0d9600 = fclk
-
-    // --- Enable basic fns in USART1 register --- //
-    uint32 TX_EN = HIGH << 3;
-    uint32 RX_EN = HIGH << 2;
-    uint32 UE_EN = HIGH << 0;
-
-    uint32 cr1_val = TX_EN | RX_EN | UE_EN;
-    USART1->CR1 &= ~cr1_val;
-    USART1->CR1 |= cr1_val;
-
-   
-    // --- CR1 --- //
-    // USART on reset:
-    // Word length: 1 st bit, 8 data bits, n stop bits
-    // No interrupts
-    // Driver enable on/off time 0
-    // Oversample by 16
-    // Unmuted
-    // Mute wakeup set to idle line
-    // No parity control
-    // Even parity
-    // Tx disabled (bit 3)
-    // Rx disabled (bit 2)
-    // USART disabled (bit 0)
-    // --- CR2 --- //
-    // Unique id 0
-    // rx timeout disabled
-    // auto baud rate: detected by start bit
-    // auto baud rate disabled
-    // MSB first: off
-    // rx/tx data where 1=High 0=Low
-    // rx pin signal Vdd=1 (idle)
-    // rx/tx swap: off
-    // LIN mode: off
-    // 1 stop bit
-    // CLK disabled (Synch or Smartcard mode only)
-    // CLK polarity: steady low
-    // CLK phase: 1st clk transistion = 1st data capture edge
-    // Last bit CLK phase: clk pulse of last bit is not output to CLK pin
-    // No interrupts
-    // 4bit unique address mode
-    // --- CR3 --- //
-    // No interrupts
-    // No smartcard auto-retry
-    // Driver enable polarity: active high
-    // Driver enable mode: disabled
-    // DMA disable on rx error: off
-    // Rx disable on overrun: on
-    // Sample method: three bit
-    // Clear to send (CTS) mode: disabled
-    // RTS mode: disabled
-    // tx DMA mode: disabled
-    // rx DMA mode: disabled
-    // smartcard mode: disabled
-    // smartcard nack mode: disabled
-    // half-duplex mode: disabled
-    // IrDA low power mode: disabled
-    // IrDA mode: disabled
-}
-
-int finish_tx(){
-    uint32 TC = HIGH << 6;
-    while((USART1->ISR & TC) != TC); // wait for transmission complete
-    return 0;
-}
-
-int putchar(unsigned char c){
-    uint32 TXE = HIGH << 7;
-    finish_tx();
-    while((USART1->ISR & TXE) != TXE); // wait for TDR to be empty
-    USART1->TDR = (uint32)c;
-    while((USART1->ISR & TXE) != TXE); // wait for TDR to be empty
-    finish_tx();
-    return c;
-}
-
-
 void delay(unsigned int ticks){
     for(int i = 0; i < ticks; i++);
 }
@@ -194,19 +86,54 @@ void print_sequence(){
     finish_tx();
 }
 
+void printstrlen(char* s, int len){
+    char* end = s + len;
+    while(s < end){
+        if(*s == '\0'){
+            break;
+        }
+        putchar(*s);
+        s++;
+    }
+    finish_tx();
+}
+
 int main(){
     init();
     init_led3();
     init_usart();
     
+    unsigned char c;
     while(1){
         set_led3_on();
-        print_sequence();
-        delay(1000000);
+        c = getchar();
+        if(c == '\r'){
+            putchar('\n');
+            putchar(c);
+        }
+        else if(c == 127){
+            putchar('\b');
+            putchar(' ');
+            putchar('\b');
+        }
+        else{
+            putchar(c);
+        }
 
         set_led3_off();
-        print_sequence();
-        delay(1000000);
+        c = getchar();
+        if(c == '\r'){
+            putchar('\n');
+            putchar(c);
+        }
+        else if(c == 127){
+            putchar('\b');
+            putchar(' ');
+            putchar('\b');
+        }
+        else{
+            putchar(c);
+        }
     }
 
     return 0;
